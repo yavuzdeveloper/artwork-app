c1f0041927600863451f5a19b0a442d6f872ee73
yavuztokus <yavuztokus2@gmail.com>
2025-05-15 21:18:11 +0100

    T-2 adding sonarcloud and search clear icon

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import ClearIcon from "@/ui/ClearIcon";
 import SearchIcon from "@/ui/SearchIcon";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -57,26 +58,33 @@ export default function Search() {
           data-testid="search-form"
         >
           <div className="relative">
+            <button type="submit" className="hover:cursor-pointer">
+              <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+            </button>
             <input
               id="search-input"
               type="text"
               value={query}
               onChange={e => setQuery(e.target.value)}
               placeholder="Search..."
-              className="w-full min-w-[150px] sm:min-w-[300px] px-4 pr-9 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 text-base"
+              className="w-full min-w-[150px] sm:min-w-[300px] pl-10 pr-9 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300 text-base"
             />
-            <button
-              type="submit"
-              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-blue-600"
-            >
-              <SearchIcon className="w-5 h-5" />
-            </button>
+
+            {query && (
+              <button
+                type="button"
+                onClick={() => setQuery("")}
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-red-600 transition-colors hover:cursor-pointer"
+              >
+                <ClearIcon />
+              </button>
+            )}
           </div>
         </form>
       ) : (
         <button
           onClick={toggleSearch}
-          className="p-2 text-gray-500 hover:text-blue-600 transition-colors"
+          className="p-2 text-gray-500 hover:text-blue-600 transition-colors hover:cursor-pointer"
           aria-label="Search"
         >
           <SearchIcon className="w-6 h-6" />
