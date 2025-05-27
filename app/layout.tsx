3de62d669cb1cef2d8164264bb0f4730d2759a59
yavuzdeveloper <73732365+yavuzdeveloper@users.noreply.github.com>
2025-05-27 15:48:10 +0100

    T-14 adding footer (#21)
    Co-authored-by: yavuztokus <yavuztokus2@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- components/ArtworkCard/ArtworkCard.tsx
+++ components/ArtworkCard/ArtworkCard.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 import { Artwork } from "@/types";
 import GridImage from "../GridImage/GridImage";
 
-export default function ArtworkCard({ artwork }: { artwork: Artwork }) {
+export default function ArtworkCard({
+  artwork,
+  priority,
+}: {
+  artwork: Artwork;
+  priority?: boolean;
+}) {
   return (
     <Link
       href={`/artwork/${artwork.id}`}
@@ -14,6 +20,7 @@ export default function ArtworkCard({ artwork }: { artwork: Artwork }) {
         <GridImage
           artwork={artwork}
           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
+          priority={priority}
         />
         <div className="absolute inset-0 bg-gradient-to-t from-black/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
       </div>
