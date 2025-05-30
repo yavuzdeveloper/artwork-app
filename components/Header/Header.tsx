6a1ead2916a7e382cf0aa59710e2fc79d578a035
yavuzdeveloper <73732365+yavuzdeveloper@users.noreply.github.com>
2025-05-30 15:52:01 +0100

    Feature/t 16 footer improvements (#24)
    * T-16 making search icon white and replacing footer icons
    
    * T-16 quick fix
    
    * T-16 fix
    
    ---------
    
    Co-authored-by: yavuztokus <yavuztokus2@gmail.com>

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 
 import SearchIcon from "@/ui/SearchIcon";
 import Search from "../Search/Search";
-import Link from "next/link";
+import Title from "./Title";
 
 export default function Header() {
   const pathname = usePathname();
@@ -15,19 +15,8 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 bg-black shadow-sm w-full">
       <div className="max-w-7xl mx-auto p-4">
-        <div
-          className={`flex items-center ${
-            isHomePage ? "justify-between" : "justify-center"
-          } gap-4`}
-        >
-          <Link
-            href={"/"}
-            className="text-xl md:text-2xl lg:text-3xl font-bold text-white whitespace-nowrap cursor-pointer"
-            data-testid="header-title"
-          >
-            Art Collection
-          </Link>
-
+        <div className="flex items-center justify-between gap-4">
+          <Title />
           {isHomePage && (
             <div className="flex-1">
               <Suspense fallback={<SearchIcon className="w-5 h-5" />}>
